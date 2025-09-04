11028ac07a99caffa718f9218456ba89f2c6744f
Роман Беспалов <bespalovroma1995@gmail.com>
2025-09-04 22:57:31 +0300

    added constants for first and second line for parse

--- personal_account/utils/constants.py
+++ personal_account/utils/constants.py
@@ -6,23 +6,38 @@ CHOICES_TYPE_WORK_SHIFT = (
     ("Сменный", "Сменный"),
     ("5/2", "5/2")
 )
+
 CHOICES_STATUS_HOLIDAY = (
     ("Запланирован", "Запланирован"),
     ("Завершен", "Завершен"),
     ("Отменен", "Отменен")
 )
+
 CHOICES_TYPE_HOLIDAY = (
     ("Отпуск без сохранения заработной платы", (
         "Отпуск без сохранения заработной платы"
     )),
     ("Ежегодный оплачиваемый", "Ежегодный оплачиваемый")
 )
+
 TIME_FORMAT = "%H:%M"
+
 TYPE_HOLIDAY = {
     "yearly": "Ежегодный оплачиваемый",
     "without_pay": "Без сохранения заработной платы"
 }
+
 TYPE_SHIFTS = {"schedule": "Сменный", "standart": "5/2"}
+
 COLUMN_FOR_GSMA = [26, 33]
 TIME_SHIFT_FOR_GSMA = 24
 HOLIDAY_FOR_GSMA = 23
+
+COLUMN_FOR_FL = [4, 11]
+TIME_SHIFT_DAY_FOR_FL = 3
+TIME_SHIFT_FOR_NIGHT_FL = 4
+HOLIDAY_FOR_FL = 2
+
+COLUMN_FOR_SL = [16, 22]
+TIME_SHIFT_FOR_SL = 14
+HOLIDAY_FOR_SL = 13
