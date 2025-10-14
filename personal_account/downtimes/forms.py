47a32fc0aec30b6de1a89869b97dde672253993d
Роман Беспалов <bespalovroma1995@gmail.com>
2025-10-14 09:54:59 +0300

    added field link_task in form

--- personal_account/downtimes/forms.py
+++ personal_account/downtimes/forms.py
@@ -6,8 +6,20 @@ class DowntimeForm(forms.ModelForm):
 
     class Meta:
         model = Downtime
-        fields = "service", "start_downtime", "end_downtime", "description"
+        fields = (
+            "service",
+            "start_downtime",
+            "end_downtime",
+            "link_task",
+            "description"
+        )
         widgets = {
-            "start_downtime": forms.DateTimeInput(format='%Y-%m-%d %H:%M', attrs={'type': 'datetime-local'}),
-            "end_downtime": forms.DateTimeInput(format='%Y-%m-%d %H:%M', attrs={'type': 'datetime-local'})
+            "start_downtime": forms.DateTimeInput(
+                format='%Y-%m-%d %H:%M',
+                attrs={'type': 'datetime-local'}
+            ),
+            "end_downtime": forms.DateTimeInput(
+                format='%Y-%m-%d %H:%M',
+                attrs={'type': 'datetime-local'}
+            )
         }
